757c47971398f703b33163f260fcf256dc21a8b1
joseeduardogaytan <joseEduardoGaytan@gmail.com>
2020-06-20 16:51:05 -0500

    fix for unix timestamp dates

--- utils/dates_parameters.py
+++ utils/dates_parameters.py
@@ -1,16 +1,8 @@
 import datetime
 import time
 
-def getDefaultDatesParams(start, end):    
-    if start is None:
-        start_date = 21600 # From the begining of the times
-    else:
-        d = datetime.datetime.strptime(start, '%Y-%m-%d')
-        start_date = time.mktime(d.timetuple())
-    if end is None:
-        end_date = int(time.time()) # now
-    else:
-        d = datetime.datetime.strptime(end, '%Y-%m-%d')
-        end_date = time.mktime(d.timetuple())
-
+def getDefaultDatesParams(start, end):
+    start_date = 21600 if start is None else start 
+    end_date = int(time.time()) if end is None else end   
+      
     return start_date, end_date
